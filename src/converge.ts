2c6f60b1371e0de9422c1e6a7014663e3ade1101
Polina Sizintseva <polina.sizintseva@flant.com>
2026-02-12 12:22:01 +0300

    fix(deploy): respect WERF_ENV
    Signed-off-by: Polina Sizintseva <polina.sizintseva@flant.com>

--- src/converge.ts
+++ src/converge.ts
@@ -3,7 +3,11 @@ import {PrepareEnvironAndRunWerfCommand} from './common'
 
 async function run(): Promise<void> {
   try {
-    process.env.WERF_ENV = core.getInput('env')
+    // with.env parameter has priority over WERF_ENV environment variable
+    const envInput = core.getInput('env')
+    if (envInput) {
+      process.env.WERF_ENV = envInput
+    }
     await PrepareEnvironAndRunWerfCommand(['converge'])
   } catch (error) {
     core.setFailed(error.message)
